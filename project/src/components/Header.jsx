07d974e3d89a8beef9b200710946022be0f7c5a7
Leonardo Vieira Carvalho <leovc2011@gmail.com>
2025-02-14 14:35:30 -0300

    Alteração link header telefone

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -59,7 +59,7 @@ const Header = () =>{
                     <div className="div-info-nav">
                         <ul>
                             <li><i className="mdi mdi-email"></i><a href="mailto:viercatech@gmail.com">viercatech@gmail.com</a></li>
-                            <li><i className="mdi mdi-phone-classic"></i><a href="https://api.whatsapp.com/send/?phone=5511967381402&text=Ol%C3%A1%20VierCa%20Tech,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">(11) 96738-1402</a></li>
+                            <li><i className="mdi mdi-phone-classic"></i><a href="tel:+5511967381402" target="_blank" rel="noopener noreferrer">(11) 96738-1402</a></li>
                         </ul>
                     </div>
                     <div className="social-info-nav">
